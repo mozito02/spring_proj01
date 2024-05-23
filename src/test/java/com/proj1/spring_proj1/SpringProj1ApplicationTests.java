6ea6dc8b75e41f50f2ab298de87358ad3d01785b
Abir Chakraborty <abirc504@gmail.com>
2024-05-24 02:40:28 +0530

    Spring proj

---
+++ src/test/java/com/proj1/spring_proj1/SpringProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj1.spring_proj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
